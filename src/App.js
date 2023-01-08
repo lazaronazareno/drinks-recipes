c37dbb8524ac3553b3a1e3cc4552db500868f084
lazaronazareno <lazaronazareno@gmail.com>
2023-01-07 23:15:31 -0300

    imported all component and context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import DrinksList from './components/DrinksList'
+import Form from './components/Form'
+import Header from './components/Header'
+import CategoryProvider from './context/CategoryContext'
+import DrinksProvider from './context/DrinksContext'
+import RecipesProvider from './context/RecipesContext'
 
 function App () {
   return (
-    <div className='App'>
-      <h1>Drinks App</h1>
-    </div>
+    <CategoryProvider>
+      <DrinksProvider>
+        <RecipesProvider>
+          <Header heading='Drinks App' />
+          <Form />
+          <DrinksList />
+        </RecipesProvider>
+      </DrinksProvider>
+    </CategoryProvider>
   )
 }
 
